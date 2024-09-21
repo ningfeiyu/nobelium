b296218115b2edb903023ec448e9e97dcb6498ea
ningfeiyu <ningfeiyu@gmail.com>
2024-09-21 20:14:03 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -51,7 +51,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: '248a4013-30b0-45bc-b9c2-1467cd0a6edc', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
